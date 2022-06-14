faf8838ff9be2afe4a6c80d7fbbcf76eb6bf297f
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-06-14 09:16:40 -0300

    fix: Add ifmain

--- src/curate.py
+++ src/curate.py
@@ -5,16 +5,21 @@ from dictionaries.all import *
 from wdcuration import add_key
 import json
 HERE = Path(__file__).parent.resolve()
-with open(f'{HERE}/../results/projects.pickle', 'rb') as handle:
-    parsed_projects = pickle.load(handle)
 
-for project in parsed_projects:
-  for software in project.projects:
-    if software not in dicts["software"]:
-      try:
-        dicts["software"] = add_key(dicts["software"], software)
-        with open(f"{HERE}/dictionaries/software.json", "w+") as f:
-          f.write(json.dumps(dicts["software"], indent=4, sort_keys=True))
+def main():
+  with open(f'{HERE}/../results/projects.pickle', 'rb') as handle:
+      parsed_projects = pickle.load(handle)
 
-      except Exception:
-        pass
+  for project in parsed_projects:
+    for software in project.projects:
+      if software not in dicts["software"]:
+        try:
+          dicts["software"] = add_key(dicts["software"], software)
+          with open(f"{HERE}/dictionaries/software.json", "w+") as f:
+            f.write(json.dumps(dicts["software"], indent=4, sort_keys=True))
+
+        except Exception:
+          pass
+
+if __name__ == "__main__":
+  main()
