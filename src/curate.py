c5ff49fc53b7c979213ad871bb59170397ae377c
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-06-17 08:51:27 -0300

    fix: Improve documentation

--- src/curate.py
+++ src/curate.py
@@ -4,22 +4,26 @@ from pathlib import Path
 from dictionaries.all import *
 from wdcuration import add_key
 import json
+
 HERE = Path(__file__).parent.resolve()
 
+
 def main():
-  with open(f'{HERE}/../results/projects.pickle', 'rb') as handle:
-      parsed_projects = pickle.load(handle)
+    # A pickle object with the classes described in scrape.py
+    with open(f"{HERE}/../results/projects.pickle", "rb") as handle:
+        parsed_projects = pickle.load(handle)
+    print(parsed_projects)
+    for project in parsed_projects:
+        for software in project.projects:
+            if software not in dicts["software"]:
+                try:
+                    dicts["software"] = add_key(dicts["software"], software)
+                    with open(f"{HERE}/dictionaries/software.json", "w+") as f:
+                        f.write(json.dumps(dicts["software"], indent=4, sort_keys=True))
 
-  for project in parsed_projects:
-    for software in project.projects:
-      if software not in dicts["software"]:
-        try:
-          dicts["software"] = add_key(dicts["software"], software)
-          with open(f"{HERE}/dictionaries/software.json", "w+") as f:
-            f.write(json.dumps(dicts["software"], indent=4, sort_keys=True))
+                except Exception:
+                    pass
 
-        except Exception:
-          pass
 
 if __name__ == "__main__":
-  main()
+    main()
--- src/scrape.py
+++ src/scrape.py
@@ -4,61 +4,61 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import pickle
-
 from dataclasses import dataclass, field
 from pathlib import Path
+from selenium import webdriver
 
 HERE = Path(__file__).parent.resolve()
+
 
 @dataclass
 class FundedProject:
     """Class for keeping track of each CZI funded projects."""
-    href: str = ''
+
+    href: str = ""
     projects: list = field(default_factory=list)
     leads: list = field(default_factory=list)
-    cycle: str = ''
+    cycle: str = ""
+
 
 def main():
-  url = "https://chanzuckerberg.com/eoss/proposals/"
+    url = "https://chanzuckerberg.com/eoss/proposals/"
 
-  # set option to be headless
-  from selenium import webdriver
-  options = webdriver.ChromeOptions()
-  options.add_argument('headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(url)
 
-  # open it, go to a website, and get results
-  driver = webdriver.Chrome(ChromeDriverManager().install())
-  driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "lxml")
+    driver.close()
 
-  html = driver.page_source
-  soup = BeautifulSoup(html, 'lxml')
-  driver.close()
+    projects = soup.find_all(("a", {"class": "list__row is-active"}), href=True)
 
-  projects = soup.find_all(("a", {"class":"list__row is-active"}), href=True)
+    def clean_up(string):
+        return [txt.strip().replace("and ", "") for txt in string.text.split(",")]
 
+    parsed_projects = []
+    for proj in projects:
+        if proj.has_attr("data-cycle"):
+            project_now = FundedProject()
+            project_now.href = proj.get("href")
+            labels = proj.find_all("span")
+            for j, label in enumerate(labels):
+                if "Project" in labels[j - 1].text:
+                    project_now.projects = clean_up(labels[j])
+                if "Lead" in labels[j - 1].text:
+                    project_now.leads = clean_up(labels[j])
+                if "Cycle" in labels[j - 1].text:
+                    project_now.cycle = clean_up(labels[j])
+            parsed_projects.append(project_now)
 
-  def clean_up(string):
-    return [txt.strip().replace("and ","") for txt in string.text.split(",")]
-  parsed_projects = []
-  for proj in projects:
-      if proj.has_attr('data-cycle'):
-        project_now = FundedProject()
-        project_now.href = proj.get('href')
-        labels = proj.find_all("span")
-        for j, label in enumerate(labels):
-          if "Project" in labels[j-1].text : 
-            project_now.projects =  clean_up(labels[j])
-          if "Lead" in labels[j-1].text : 
-            project_now.leads = clean_up(labels[j])
-          if "Cycle" in labels[j-1].text : 
-            project_now.cycle = clean_up(labels[j]) 
-        parsed_projects.append(project_now)
+    print(parsed_projects)
+    with open(f"{HERE}/../results/projects.pickle", "wb") as handle:
+        pickle.dump(parsed_projects, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-  print(parsed_projects)
-  with open(f'{HERE}/../results/projects.pickle', 'wb') as handle:
-      pickle.dump(parsed_projects, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
-  main()
+    main()
